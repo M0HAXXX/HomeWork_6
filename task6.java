ba5bff7c0bfc91b7f6e515889ac95acaae23c5b4
M0HAXXX <95334628+M0HAXXX@users.noreply.github.com>
2022-12-13 12:16:20 +0300

    Update task6.java

--- task6.java
+++ task6.java
@@ -1,1 +1,52 @@
-
+public class Main
+{
+	public static void main(String[] args) {
+		Dog Muxa = new Dog("Мухтар");
+		Muxa.run(100);
+		Muxa.swim(100);
+		Muxa.jump(100);
+		Cat Murz = new Cat("Мурзик");
+		Murz.run(150);
+		Murz.swim(150);
+		Murz.jump(150);
+	}
+}
+class Animal{
+    String name;
+    public Animal(String name){
+        this.name = name;
+    }
+}
+class Dog extends Animal{
+    public Dog(String name){
+        super(name);
+    }
+    public void run(int length){
+        boolean a = length < 500;
+        System.out.println ("run:" + a);
+    }
+    public void swim(int length){
+        boolean a = length < 10;
+        System.out.println("swim:" + a);
+    }
+    public void jump(int height){
+        boolean a = height < 0.5;
+        System.out.println("jump:" + a);
+    }
+}
+class Cat extends Animal{
+    public Cat(String name){
+        super(name);
+    }
+    public void run(int length){
+        boolean a = length < 200;
+        System.out.println ("run:" + a);
+    }
+    public void swim(int length){
+        System.out.println("swim:" + false);
+    }
+    public void jump(int height){
+        boolean a = height < 2;
+        System.out.println("jump:" + a);
+    }
+}
